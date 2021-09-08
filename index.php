e2aebc48b3679467fe209eae5a13db39ec44dfb9
Akhil M Anil <70442264+akhil-1998@users.noreply.github.com>
2021-09-08 12:09:22 +0530

    Index page styling

--- index.php
+++ index.php
@@ -1,53 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<title style="times-new-roman">Horizon</title>
+		<title>Horizon</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
 		<link type="text/css" rel="stylesheet" href="main.css">
 
-		<style>
-			h1 {
-				display: block;
-				font-size: 90px;
-				margin-top: 0.67em;
-				margin-bottom: 0.67em;
-				margin-left: 0;
-				margin-right: 0;
-				font-weight: bold;
-				color: rgb(245, 218, 63);
-				}
-			h2 {
-				display: block;
-				font-size: 50px;
-				margin-top: 0.67em;
-				margin-bottom: 0.67em;
-				margin-left: 0;
-				margin-right: 0;
-				font-weight: bold;
-				}
-			a:link, a:visited {
-					background-color: #fcfcfc;
-					color: rgb(8, 8, 8);
-					padding: 20px 33px;
-					text-align: center;
-					text-decoration: none;
-					display: inline-block;
-					}
 
-			a:hover, a:active {
-					background-color: rgb(250, 250, 250);
-					}	
-		</style>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r79/three.min.js"></script>
 	</head>
 
 	<body>
-		<div id="info" style="font-family:Comic Sans MS">
-			<h1 >Horizon</h1><h2>The Virtual Classroom</h2><br/>
-			<a href= "teacherlogin.php">Teacher</a>
-			<a href= "studentlogin.php">Student</a>
-
+		<div id="info">
+			<div class="center">
+			<!-- <img src="images/logo.png" width="200" height="100"alt="horizon" > -->
+			<h1>Horizon</h1><h2>The Virtual Classroom</h2><br/>
+			<a href= "teacherlogin.php" class="btn-google m-b-20">
+			Teacher
+			</a>
+			<a href= "studentlogin.php" class="btn-google m-b-20">
+			Student
+			</a>
+			</div>
 		</div>
 
 		<script type="module">
